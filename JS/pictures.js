c72b50164efd588e8f0a1d48353968720650ce15
Karolina <karolina.limanowska@gmail.com>
2023-12-01 12:41:56 +0100

    making loader to work

--- JS/pictures.js
+++ JS/pictures.js
@@ -1,4 +1,7 @@
+const loader = document.getElementById('loader');
+
 function fetchPicture() {
+    loader.className='showLoader';
     fetch('https://api.thecatapi.com/v1/images/search?limit=1')
         .then((response) => {
             return response.json();
@@ -10,12 +13,13 @@ function fetchPicture() {
 //NOTE use console.log() when something doesn't work
 
 function displayCatPicture(catPicture) {
-  var container = document.getElementById('catPictureContainer');
-  var image = document.createElement('img');
-  image.src = catPicture[0].url; //catPictures is an object with an array of images so need to point towards the first instance of the array
-  image.alt = 'Cat Picture';
-  container.replaceChildren();
-  container.appendChild(image);
+    var container = document.getElementById('catPictureContainer');
+    var image = document.createElement('img');
+    image.src = catPicture[0].url; //catPictures is an object with an array of images so need to point towards the first instance of the array
+    image.alt = 'Cat Picture';
+    container.replaceChildren();
+    container.appendChild(image);
+    loader.className='';
 }
 
 fetchPicture();
